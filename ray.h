ca72332a5556630d7d6f660a093e5eafa60d55c2
Christian Schladetsch <christian.schladetsch@gmail.com>
2024-09-29 18:14:10 +1000

    Cleaning up unused fles.

--- ray.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "vec3.h"
-
-struct Ray {
-    Vec3 origin, direction;
-    __host__ __device__ Ray(const Vec3& origin, const Vec3& direction) : origin(origin), direction(direction) {}
-};
